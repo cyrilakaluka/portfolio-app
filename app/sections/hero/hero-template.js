6ff0d49f96f0cd8d943e3a2b2c065470b816daa5
Cyril Akaluka <cyril.akaluka@gamesglobal.com>
2024-08-09 21:22:53 +0300

    Set root font size configuration

--- app/sections/hero/hero-template.js
+++ app/sections/hero/hero-template.js
@@ -1,61 +1,61 @@
 const css = /*css*/`
   :host {
-    display: block;
-    height: calc(100vh - var(--nav-height));
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    min-height: 100dvh;
   }
 
-  :host::before {
-    content: '';
-    display: block;
-    height: var(--nav-height);
+  .nav-offset {
+    min-height: var(--nav-height);
   }
 
-  app-media-block {
-    height: 100%;
+  .content {
+    flex: 1;
   }
 
   /* 
    * content section
   */
   .greeting {
-    font-size: 32px;
+    font-size: 3.2rem;
     color: var(--accent-color);
     margin-bottom: 16px;
   }
 
   .name {
-    font-size: 72px;
+    font-size: 7.2rem;
     text-transform: uppercase;
-    margin-bottom: 24px;
+    margin-bottom: 2.4rem;
   }
 
   .job-role {
-    font-size: 32px;
+    font-size: 3.2rem;
     color: var(--accent-color);
-    margin-bottom: 16px;
+    margin-bottom: 1.6rem;
   }
 
   .summary {
-    font-size: 20px;
+    font-size: 2rem;
     opacity: 0.7;
     text-align: justify;
-    margin-bottom: 40px;
+    margin-bottom: 4rem;
   }
 
   .buttons {
     display: flex;
-    gap: 15px;
-    margin-bottom: 40px;
+    gap: 1.5rem;
+    margin-bottom: 4rem;
   }
 
   .socials {
     display: flex;
-    gap: 20px;
+    gap: 2rem;
     width: min-content;
-    padding: 20px;
+    padding: 2rem;
     background-color: rgba(255, 255, 255, 0.05);
-    border-radius: 50px;
-    border: 1px solid #232935;
+    border-radius: 5rem;
+    border: 0.1rem solid #232935;
   }
 
   .socials:hover {
@@ -77,7 +77,7 @@ const css = /*css*/`
     width: 100%;
     aspect-ratio: 1 / 1;
     border-radius: 100%;
-    border: 10px solid var(--accent-color);
+    border: 1rem solid var(--accent-color);
     overflow: hidden;
   }
 
@@ -88,7 +88,7 @@ const css = /*css*/`
     width: 100%;
     aspect-ratio: 1 / 1;
     border-radius: 100%;
-    border: 5px solid var(--background-color);
+    border: 0.5rem solid var(--background-color);
   }
 
   .frame > img {
@@ -98,7 +98,8 @@ const css = /*css*/`
 
 export default  /*html*/`
   <style>${css}</style>
-  <app-media-block block-title="Hero" image-url="./assets/hero.jpg" image-frame="rounded" content-size-ratio="1.5">
+  <div class="nav-offset"></div>
+  <app-media-block class="content" block-title="Hero" image-url="./assets/hero.jpg" image-frame="rounded" content-size-ratio="1.5">
     <div class="text">
       <h3 class="greeting">Hey there!, I'm</h3>
       <h1 class="name">Cyril Akaluka</h1>
