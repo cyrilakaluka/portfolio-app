4261cd22185cb700b3d01e2e75134395349a1e12
Cyril Akaluka <cyril.akaluka@gamesglobal.com>
2024-08-10 19:42:32 +0300

    removed margin between hero and next section

--- app/components/layout/layout-template.js
+++ app/components/layout/layout-template.js
@@ -1,39 +1,73 @@
+const css = /*css*/`
+  :host {
+    margin: auto;
+    position: relative;
+    display: block;
+    width: 100%;
+  }
+
+  slot {
+    width: inherit;
+    max-width: inherit;
+  }
+  :host {
+    margin: auto;
+    position: relative;
+    display: block;
+    width: 100%;
+  }
+
+  slot {
+    width: inherit;
+    max-width: inherit;
+  }
+  :host {
+    margin: auto;
+    position: relative;
+    display: block;
+    width: 100%;
+  }
+
+  slot {
+    width: inherit;
+    max-width: inherit;
+  }
+  :host {
+    margin: auto;
+    position: relative;
+    display: block;
+    width: 100%;
+  }
+
+  slot {
+    width: inherit;
+    max-width: inherit;
+  }
+
+  .container {
+    max-width: 1200px;
+    padding-left: 20px;
+    padding-right: 20px;
+    margin: auto;
+  }
+
+  .header {
+    position: fixed;
+    width: 100%;
+    background-color: var(--background-color);
+    box-shadow: 0 10px 20px rgba(255, 255, 255, 0.2);
+    z-index: 99999;
+  }
+
+  .sections {
+    display: flex;
+    flex-direction: column;
+    gap: 200px;
+  }
+`;
+
 export default /*html*/`
-  <style>
-    :host {
-      margin: auto;
-      position: relative;
-      display: block;
-      width: 100%;
-    }
-
-    slot {
-      width: inherit;
-      max-width: inherit;
-    }
-
-    .container {
-      max-width: 1200px;
-      padding-left: 20px;
-      padding-right: 20px;
-      margin: auto;
-    }
-
-    .header {
-      position: fixed;
-      width: 100%;
-      background-color: var(--background-color);
-      box-shadow: 0 10px 20px rgba(255, 255, 255, 0.2);
-      z-index: 99999;
-    }
-
-    .sections {
-      display: flex;
-      flex-direction: column;
-      margin-top: 100px;
-      gap: 200px;
-    }
-  </style>
+  <style>${css}</style>
   <header class="header">
     <div class="container">
       <slot name="header"></slot>
